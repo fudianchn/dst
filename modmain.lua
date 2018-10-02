005ceeafb5b4c41dd2ce164dca50a7adc78dd6a9
fudian <fudianchn@gmail.com>
2018-10-02 12:01:21 +0800

    汉化更新
    汉化更新

--- modmain.lua
+++ modmain.lua
@@ -4,8 +4,8 @@ LoadPOFile("guido.po", "chs")
 
 if _G.debug.getupvalue(string.match, 1) == nil then
     -- 修复因游戏原因无法正确匹配中文
-    local fr_old_match = string.match
+    local oldmatch = string.match
     function string.match(str, pattern, index)
-        return fr_old_match(str, pattern:gsub("%%w", "[%%w一-鿕]"), index)
+        return oldmatch(str, pattern:gsub("%%w", "[%%w一-鿕]"), index)
     end
 end
