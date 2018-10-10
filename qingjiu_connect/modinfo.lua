02204c68e0046a5ec5420eeb1b0e8fc510252fd4
fudian <fudianchn@gmail.com>
2018-10-11 03:43:54 +0800

    直连补丁
    直连

---
+++ qingjiu_connect/modinfo.lua
@@ -0,0 +1,17 @@
+name = "清酒直连"
+description = "自用"
+author = "Guido"
+version = "0.2.0"
+forumthread = ""
+api_version = 10
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+shipwrecked_compatible = false
+dst_compatible = true
+client_only_mod = true
+all_clients_require_mod = false
+server_filter_tags = {"Guido"}
+
+priority = -99
